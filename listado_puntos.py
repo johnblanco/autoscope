3a9a5f481c085d09d62309b390eaa46218a5c87f
johnblanco <j.pabloblanco@gmail.com>
2018-08-05 17:44:01 -0300

    Update listado_puntos.py

--- listado_puntos.py
+++ listado_puntos.py
@@ -17,7 +17,10 @@ with open('autoscope_agosto_2017.csv', encoding="latin-1") as f:
         }
 
         clave = d['avenida'] + '-' + d['esquina1'] + '-' + d['esquina2']
-        puntos[clave] = d['lat'] + ',' + d['long']
+        puntos[clave] = {'coordenadas': d['lat'] + ' ' + d['long'],
+                         'avenida': d['avenida'],
+                         'esquina1': d['esquina1'],
+                         'esquina2': d['esquina2']}
 
         if i % 1000000:
             print(i)
@@ -25,8 +28,10 @@ with open('autoscope_agosto_2017.csv', encoding="latin-1") as f:
         i += 1
 
 f2 = open('puntos.csv', 'w')
+f2.write('avenida,esquina 1,esquina 2, coordenadas\n')
 for p in puntos.keys():
-    linea = p + ';' + puntos[p]
-    print(linea)
+    linea = '{},{},{},{}'.format(puntos[p]['avenida'],puntos[p]['esquina1'],puntos[p]['esquina2'],
+                      puntos[p]['coordenadas'])
     f2.write(linea+"\n")
+    print(linea)
 f2.close()
